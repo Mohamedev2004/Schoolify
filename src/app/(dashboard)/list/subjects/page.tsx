40e87368942a2ba8163060b98c19c7eae7d9af2f
mohammed <mohamedbaya30@gmail.com>
2025-03-12 19:54:01 +0000

    12/03/2025

---
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -0,0 +1,78 @@
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import Table from "@/components/Table";
+
+import Image from "next/image";
+import { role, subjectsData} from "@/lib/data";
+import Link from "next/link";
+
+
+type Subject = {
+  id:number;
+  name:string;
+  teachers:string[];
+}
+
+const columns = [
+  {header:"Subject Name", accessor:"name"},
+  {header:"Teachers", accessor:"teachers", className:"hidden md:table-cell"},
+  {header:"Actions", accessor:"action"},
+
+];
+
+
+const SubjectListPage = () => {
+
+
+  const renderRow = (item:Subject) => (
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm even:hover:bg-school even:hover:bg-opacity-50 odd:hover:bg-schoolify odd:hover:bg-opacity-50">
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 rounded-full flex items-center justify-center bg-schoolify">
+              <Image src="/edit.png" alt="" width={16} height={16}/>
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-8 h-8 rounded-full flex items-center justify-center bg-school">
+              <Image src="/delete.svg" alt="" width={16} height={16}/>
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch/>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-school">
+              <Image src="/filter.png" alt="" width={14} height={14}/>
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-school">
+              <Image src="/sort.png" alt="" width={14} height={14}/>
+            </button>
+            {role === "admin" && (
+                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-school">
+                    <Image src="/plus.png" alt="" width={14} height={14}/>
+                </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* List */}
+      <Table columns={columns} renderRow={renderRow} data={subjectsData}/>  
+      {/* Pagination */}
+      <Pagination/>
+    </div>
+  )
+}
+
+export default SubjectListPage;
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,23 +1,34 @@
 "use client"
 
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, View, Views, momentLocalizer } from "react-big-calendar";
+import moment from 'moment';
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { calendarEvents } from '@/lib/data';
+import { useState } from "react";
 
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => (
-  <div>
+const BigCalendar = () => {
+  const[view,setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView:View)=>{
+    setView(selectedView);
+  }
+  return (
     <Calendar
       localizer={localizer}
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      views={["work_week","day"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      min={new Date(2025,1,0,8,0,0)}
+      max={new Date(2025,1,0,18,0,0)}
     />
-  </div>
-)
+  )
+}
 
 export default BigCalendar;
