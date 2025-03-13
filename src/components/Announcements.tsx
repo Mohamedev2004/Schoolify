f04b58f5d08060c0d6da83094bb6ec15b2136eb5
mohammed <mohamedbaya.dev@gmail.com>
2025-03-13 09:41:29 +0000

    update

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -8,21 +8,21 @@ const Announcements = () => {
         <div className="flex flex-col gap-4 mt-4">
             <div className="bg-[rgba(133,125,227,0.3)] rounded-md p-4">
                 <div className="flex items-center justify-between">
-                    <h2 className="font-medium">New Feature Release: Dark Mode</h2>
+                    <h2 className="font-semibold text-gray-600">New Feature Release: Dark Mode</h2>
                     <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1 whitespace-nowrap">2025-01-01</span>
                 </div>
                 <p className="text-sm text-gray-500 mt-2">We've added dark mode to enhance your browsing experience. Try it now in settings!</p>
             </div>
             <div className="bg-[rgba(250,210,44,0.3)] rounded-md p-4">
                 <div className="flex items-center justify-between">
-                    <h2 className="font-medium">Bug Fixes & Performance Updates</h2>
+                    <h2 className="font-semibold text-gray-600">Bug Fixes & Performance Updates</h2>
                     <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1 whitespace-nowrap">2025-01-01</span>
                 </div>
                 <p className="text-sm text-gray-500 mt-2">We've fixed minor bugs and improved performance for a smoother experience.</p>
             </div>
             <div className="bg-[rgba(133,125,227,0.3)] rounded-md p-4">
                 <div className="flex items-center justify-between">
-                    <h2 className="font-medium">Welcome to Our New Members!</h2>
+                    <h2 className="font-semibold text-gray-600">Welcome to Our New Members!</h2>
                     <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1 whitespace-nowrap">2025-01-01</span>
                 </div>
                 <p className="text-sm text-gray-500 mt-2">We are excited to welcome new users to our platform. Stay tuned for upcoming features!</p>
--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -21,7 +21,7 @@ const events = [
   {
     id: 2,
     title: "React Summit",
-    time: "12:00 PM - 02:00 PM",
+    time: "13:00 PM - 01:00 PM",
     description: "A must-attend event for React and Next.js enthusiasts, featuring expert talks.",
   },
   {
@@ -43,11 +43,17 @@ const EventCalendar = () => {
         <Image src="/moreDark.png" alt="" width={20} height={20}/>
       </div>
       <div className="flex flex-col gap-4">
-        {events.map(event=>(
-          <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-schoolify even:border-t-school" key={event.id}>
+        {events.map((event, index) => (
+          <div className="p-5 rounded-md border-2 border-gray-100 border-t-4" key={event.id}>
             <div className="flex items-center justify-between">
-              <h1 className="font-semibold text-gray-600">{event.title}</h1>
-              <span className="text-gray-400 text-xs">{event.time}</span>
+              <h1 className="font-semibold text-gray-600 text-sm">{event.title}</h1>
+              <span
+                className={`text-xs text-white whitespace-nowrap rounded-md px-2 py-1 ${
+                  index % 2 === 0 ? "bg-schoolify" : "bg-school"
+                }`}
+              >
+                {event.time}
+              </span>
             </div>
             <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
           </div>
