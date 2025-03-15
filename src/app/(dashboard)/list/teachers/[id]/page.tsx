7b1b4696dc558498f80a4757d2f48c6969f371e3
mohammed <mohamedbaya.dev@gmail.com>
2025-03-15 16:40:58 +0000

    All forms, front-end done 100%

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -14,43 +14,48 @@ const SingleTeacherPage = () => {
             {/* Top */}
             <div className="flex flex-col lg:flex-row gap-4">
                 {/* User Info Card */}
-                <div className="bg-school py-6 px-4 rounded-md flex-1 flex gap-4">
+                <div className="bg-schoolify py-6 px-4 rounded-md flex-1 flex gap-4">
                     <div className="w-1/3">
                         <Image src="/teacher.jpg" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover"/>
                     </div>
                     <div className="w-2/3 flex flex-col justify-between gap-4">
                     <div className="flex items-center gap-4">
-                        <h1 className="text-xl font-semibold">Mary Jane</h1>
+                        <h1 className="text-xl font-semibold text-white">Mary Jane</h1>
                             <FormModal table="teacher" type="update" data={{
                                 id: 1,
-                                teacherId: "1234567890",
-                                name: "John Doe",
+                                username: "John Doe",
                                 email: "john@doe.com",
-                                photo:
+                                password: "password",
+                                firstName: "John",
+                                lastName: "Doe",
+                                img:
                                 "https://images.pexels.com/photos/2888150/pexels-photo-2888150.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                                bloodType: "A+",
                                 phone: "1234567890",
                                 subjects: ["Math", "Geometry"],
                                 classes: ["1B", "2A", "3C"],
+                                sex: "male",
+                                birthday: "2000-01-01",
                                 address: "123 Main St, Anytown, USA",
                             }}/>
                     </div>
-                        <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <p className="text-sm text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                         <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <Image src="/blood.png" alt="" width={14} height={14}/>
-                                <span>A+</span>
+                                <span className="text-school">A+</span>
                             </div>
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <Image src="/date.png" alt="" width={14} height={14}/>
-                                <span>March 2025</span>
+                                <span className="text-school">March 2025</span>
                             </div>
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <Image src="/mail.png" alt="" width={14} height={14}/>
-                                <span>meryjane@gmail.com</span>
+                                <span className="text-school">meryjane@gmail.com</span>
                             </div>
                             <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                                 <Image src="/phone.png" alt="" width={14} height={14}/>
-                                <span>0606060606</span>
+                                <span className="text-school">0606060606</span>
                             </div>
                         </div> 
                     </div>
