7182d0d51c30fd22b16aad33cee6f8019e5e42af
mohammed <mohamedbaya.dev@gmail.com>
2025-03-16 16:41:43 +0000

    fetching parents data

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -85,6 +85,9 @@ const StudentListPage = async ({
             break;
           case "search":
             query.name = {contains: value, mode:"insensitive"};
+            break;
+          default:
+            break;
         }
       }
     }
