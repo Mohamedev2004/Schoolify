388e707385d30f1ee82838d888a6c901db05d6b8
mohammed <mohamedbaya.dev@gmail.com>
2025-03-19 14:06:29 +0000

    fetching events data

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -6,19 +6,13 @@ import Image from "next/image";
 import { role, eventsData} from "@/lib/data";
 import Link from "next/link";
 import FormModal from "@/components/FormModal";
+import { Class, Event, Prisma } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
 
 
 
-type Event = {
-  id:number;
-  title:string;
-  class:string;
-  date:string;
-  startTime:string;
-  endTime:string;
-  
- 
-}
+type EventList = Event & { class: Class};
 
 const columns = [
   {header:"Title", accessor:"title"},
@@ -31,32 +25,81 @@ const columns = [
 
 ];
 
+const renderRow = (item:EventList) => (
+  <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm even:hover:bg-school even:hover:bg-opacity-50 odd:hover:bg-schoolify odd:hover:bg-opacity-50">
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-Us").format(item.startTime)}</td>
+    <td className="hidden md:table-cell">{item.startTime.toLocaleTimeString("en-Us",
+        {hour:"2-digit",
+        minute:"2-digit",
+        hour12: false,}
+      )}
+    </td>
+    <td className="hidden md:table-cell">{item.endTime.toLocaleTimeString("en-Us",
+        {hour:"2-digit",
+        minute:"2-digit",
+        hour12: false,}
+      )}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          /* <button className="w-8 h-8 rounded-full flex items-center justify-center bg-school">
+            <Image src="/delete.svg" alt="" width={16} height={16}/>
+          </button> */
+          <>
+            <FormModal table={"event"} type={"update"} data={item}/>
+            <FormModal table={"event"} type={"delete"} id={item.id}/>
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
-const EventListPage = () => {
+
+const EventListPage = async ({
+  searchParams,
+}: {
+  searchParams:{[key:string]:string | undefined};
+}) => {
+
+  const {page, ...queryParams} = searchParams
+
+  const p = page ? parseInt(page) : 1;
+
+  /*   URL PARAMS CONDITIONS */
+
+  const query: Prisma.EventWhereInput = {}
+
+  if (queryParams){
+    for(const [key,value] of Object.entries(queryParams)){
+      if(value !== undefined) {
+        switch(key){
+            case "search":
+              query.title = {contains:value, mode:"insensitive"};
+              break;
+            default:
+              break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.event.findMany({
+      where: query,
+      include: {
+        class:true
+      },
+      take:ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1)
+    }),
+    prisma.event.count({where:query}),
+  ]);
 
 
-  const renderRow = (item:Event) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm even:hover:bg-school even:hover:bg-opacity-50 odd:hover:bg-schoolify odd:hover:bg-opacity-50">
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td className="hidden md:table-cell">{item.startTime}</td>
-      <td className="hidden md:table-cell">{item.endTime}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            /* <button className="w-8 h-8 rounded-full flex items-center justify-center bg-school">
-              <Image src="/delete.svg" alt="" width={16} height={16}/>
-            </button> */
-            <>
-              <FormModal table={"event"} type={"update"} data={item}/>
-              <FormModal table={"event"} type={"delete"} id={item.id}/>
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+  
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -83,9 +126,9 @@ const EventListPage = () => {
         </div>
       </div>
       {/* List */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData}/>  
+      <Table columns={columns} renderRow={renderRow} data={data}/>  
       {/* Pagination */}
-      <Pagination/>
+      <Pagination page={p} count={count}/>
     </div>
   )
 }
