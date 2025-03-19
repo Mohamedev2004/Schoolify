0247bd5b7c749332a0f52f9866d00280922a654f
mohammed <mohamedbaya.dev@gmail.com>
2025-03-19 14:20:26 +0000

    fetching announcement data

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -6,17 +6,12 @@ import Image from "next/image";
 import { role, announcementsData} from "@/lib/data";
 import Link from "next/link";
 import FormModal from "@/components/FormModal";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import prisma from "@/lib/prisma";
+import { Announcement, Class, Prisma } from "@prisma/client";
 
 
-type Announcement = {
-  id:number;
-  title:string;
-  class:string;
-  date:string;
-
-  
- 
-}
+type AnnouncementList = Announcement & {class: Class}
 
 const columns = [
   {header:"Title", accessor:"title"},
@@ -27,30 +22,70 @@ const columns = [
 
 ];
 
+const renderRow = (item:AnnouncementList) => (
+  <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm even:hover:bg-school even:hover:bg-opacity-50 odd:hover:bg-schoolify odd:hover:bg-opacity-50">
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-Us").format(item.date)}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          /* <button className="w-8 h-8 rounded-full flex items-center justify-center bg-school">
+            <Image src="/delete.svg" alt="" width={16} height={16}/>
+          </button> */
+          <>
+            <FormModal table={"announcement"} type={"update"} data={item}/>
+            <FormModal table={"announcement"} type={"delete"} id={item.id}/>
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
-const AnnouncementListPage = () => {
+
+const AnnouncementListPage = async ({
+  searchParams,
+}: {
+  searchParams:{[key:string]:string | undefined};
+}) => {
+
+  const {page, ...queryParams} = searchParams
+
+  const p = page ? parseInt(page) : 1;
+
+  /*   URL PARAMS CONDITIONS */
+
+  const query: Prisma.AnnouncementWhereInput = {}
+
+  if (queryParams){
+    for(const [key,value] of Object.entries(queryParams)){
+      if(value !== undefined) {
+        switch(key){
+            case "search":
+              query.title = {contains:value, mode:"insensitive"};
+              break;
+            default:
+              break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.announcement.findMany({
+      where: query,
+      include: {
+        class:true
+      },
+      take:ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1)
+    }),
+    prisma.announcement.count({where:query}),
+  ]);
 
 
-  const renderRow = (item:Announcement) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm even:hover:bg-school even:hover:bg-opacity-50 odd:hover:bg-schoolify odd:hover:bg-opacity-50">
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            /* <button className="w-8 h-8 rounded-full flex items-center justify-center bg-school">
-              <Image src="/delete.svg" alt="" width={16} height={16}/>
-            </button> */
-            <>
-              <FormModal table={"announcement"} type={"update"} data={item}/>
-              <FormModal table={"announcement"} type={"delete"} id={item.id}/>
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+ 
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -76,9 +111,9 @@ const AnnouncementListPage = () => {
         </div>
       </div>
       {/* List */}
-      <Table columns={columns} renderRow={renderRow} data={announcementsData}/>  
+      <Table columns={columns} renderRow={renderRow} data={data}/>  
       {/* Pagination */}
-      <Pagination/>
+      <Pagination page={p} count={count}/>
     </div>
   )
 }
